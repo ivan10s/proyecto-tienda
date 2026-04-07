bb45514abaab9f15d4efc86355779bb0745ca366
ivan10s <federicosea95@gmail.com>
2026-04-07 10:05:34 -0300

    Add files via upload

---
+++ src/app/auth/inicios/inicios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inicios',
+  imports: [],
+  templateUrl: './inicios.component.html',
+  styleUrl: './inicios.component.css'
+})
+export class IniciosComponent {
+
+}
